034f0eb97e7c0c0842c4680fe1c6a9a24a9101fe
nalini <nalinityagi2010@gmail.com>
2024-08-13 13:47:45 +0530

    git demo

---
+++ src/main/java/com/excelr/JwtMorning/JwtMorningApplication.java
@@ -0,0 +1,15 @@
+package com.excelr.JwtMorning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtMorningApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(JwtMorningApplication.class, args);
+		System.out.println("rama");
+	}
+
+}
---
+++ src/test/java/com/excelr/JwtMorning/JwtMorningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelr.JwtMorning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtMorningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
